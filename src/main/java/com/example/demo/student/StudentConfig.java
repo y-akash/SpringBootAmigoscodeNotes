e3243e9e14dd88815783330bab06cea8a1d565e7
i_am_akash_ <ay157311@gmail.com>
2021-02-16 20:47:20 +0530

    used @Configuration and @Bean
    Created a StudentConfig class in student package and here write the logic for saving the student details

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+
+            Student akash = new Student(
+                    "Akash",
+                    "ay@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    22
+            );
+
+            Student anubhav = new Student(
+                    "Anubhav",
+                    "ay@gmail.com",
+                    LocalDate.of(2005, Month.JANUARY, 5),
+                    22
+            );
+
+            repository.saveAll(
+                    List.of(akash, anubhav)
+            );
+
+        };
+    }
+}
