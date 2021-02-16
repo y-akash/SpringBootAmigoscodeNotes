33c36ea603ac66129b4c50ee3e2d864b6fe3f5a9
i_am_akash_ <ay157311@gmail.com>
2021-02-16 20:29:18 +0530

    getting all student data from database
    created StudentRepository interface and injected in the StudentService constructor and in that class from getStudet method we call the findAll method using studentRepository object

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,15 +10,14 @@ import java.util.List;
 // to use @Autowired
 @Service
 public class StudentService {
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudent(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Akash",
-                        "ay@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        22
-                )
-        );
+        return studentRepository.findAll();
     }
 }
