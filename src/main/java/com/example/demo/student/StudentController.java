5996b41910f3cfd8f3c77f2ae337e67680946680
i_am_akash_ <ay157311@gmail.com>
2021-02-17 12:22:25 +0530

    used post request
    inside StudentController class we created registerNewStudent() method and applied annotation of @PostMapping so that if we trigger post request tha registerNewStudent method will run and also we used @RequestBody annotation using that we can get the data  which we are passing via post request and it will map to the Student class

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +26,10 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudent(){
         return  studentService.getStudent();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {  // we take a @RequestBody data and map to Student class
+        studentService.addNewStudent(student);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -20,4 +20,8 @@ public class StudentService {
     public List<Student> getStudent(){
         return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+    }
 }
