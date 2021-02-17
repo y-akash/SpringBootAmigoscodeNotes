b21a3ab20fd84f482a14ac80a3905da5c2cc09ac
i_am_akash_ <ay157311@gmail.com>
2021-02-17 13:06:05 +0530

    insert data based on condition
    inside StudentRepository interface we write Optional query so that we can check whether the email is present or not and inside application.properties file we add server.error wala part so that when we throw error message so it will be include that message

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+//    @Query("SELECT s FROM s WHERE s.email = ?1")  //this is not a straight sql, it's a jbql
+    Optional<Student> findStudentByEmail(String email); //this line will transform to above sql underhood
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 // to use @Autowired
 @Service
@@ -22,6 +23,10 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Email Taken");
+        }
+        studentRepository.save(student);
     }
 }
